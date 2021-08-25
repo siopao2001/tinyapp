74adc3d09781247a99a6006671da015ae2780104
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-25 16:18:48 +0000

    Add another path

--- express_server.js
+++ express_server.js
@@ -6,15 +6,19 @@ const PORT = 8080
 const urlDatabase = urlDatabse = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get('/', (require, response)=>{
    response.send('Hello!');
-})
+});
 
 app.get('/urls.json', (require, response)=>{
    response.json(urlDatabase);
 })
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, ()=>{
  console.log(`Example app listening on port ${PORT}`)
