7ffd2f8639266dc538dc7948684dd52ca10088e5
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-25 16:10:08 +0000

    Create initial server functions

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 8080
+
+const urlDatabase = urlDatabse = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get('/', (require, response)=>{
+   response.send('Hello!');
+})
+
+app.listen(PORT, ()=>{
+ console.log(`Example app listening on port ${PORT}`)
+})
