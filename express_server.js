c5394a72b830a6cd23df2c97ea0814644c71b37c
siopao2001 <sio_pao2001@yahoo.ca>
2021-09-01 03:19:48 +0000

    Modified code to add more permission features

--- express_server.js
+++ express_server.js
@@ -28,6 +28,19 @@ function emailSearch(userEmail) {
   return false
 };
 
+function urlsForUser(id) {
+  let newObject = {}
+  for (url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      newObject[url] = {
+        longURL: urlDatabase[url].longURL,
+        userID: urlDatabase[url].userID
+      }
+    }
+  }
+  return newObject
+};
+
 app.set('view engine', 'ejs')
 
 const urlDatabase =  {};
@@ -58,7 +71,7 @@ app.post('/login', (require, response)=>{
 
 app.post('/logout', (require, response)=>{
    response.clearCookie('user_id')
-   response.redirect(`/urls`)
+   response.redirect(`/login`)
 })
 
 app.get('/register', (require, response)=>{
@@ -83,9 +96,16 @@ app.post('/register', (require, response) => {
   }
 })
 
-app.get('/urls', (require, response)=>{
-   const templateVars = {urls: urlDatabase, user: users[require.cookies["user_id"]]}
-   response.render('urls_index', templateVars);
+app.get('/urls', (require, response) => {
+  if (!users[require.cookies["user_id"]]) {
+    response.send("Please log in or register")
+  } else {
+    const templateVars = {
+      urls: urlsForUser(require.cookies["user_id"]),
+      user: users[require.cookies["user_id"]]
+    }
+    response.render('urls_index', templateVars);
+  }
 })
 
 app.get('/urls/new', (require, response) => {
@@ -112,14 +132,22 @@ app.post('/urls/:id', (require, response)=>{
 })
 
 app.post('/urls/:shortURL/delete', (require, response)=>{
+   if(!users[require.cookies["user_id"]] || urlDatabase[require.params.shortURL].userID !== users[require.cookies["user_id"]].id){
+   response.send("Please login as the appropriate user")
+ }else{
    const shURL = require.params.shortURL
    delete urlDatabase[shURL]
    response.redirect(`/urls`)
+ }
 })
 
 app.get("/urls/:shortURL", (req, res) => {
+  if(!users[req.cookies["user_id"]] || urlDatabase[req.params.shortURL].userID !== users[req.cookies["user_id"]].id){
+  res.send("Please login as the appropriate user")
+ }else{
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies["user_id"]]};
   res.render("urls_show", templateVars);
+ }
 });
 
 app.get("/u/:shortURL", (req, res) => {
