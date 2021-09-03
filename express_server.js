4432b47e6e283e85bed3e02e8d4a9ededecdcf47
siopao2001 <sio_pao2001@yahoo.ca>
2021-09-03 11:50:01 +0000

    Installed cookies-session and modified routes to use encrypted cookies

--- express_server.js
+++ express_server.js
@@ -6,14 +6,22 @@ const PORT = 8080;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['PAOLO'],
+  maxAge: 24 * 60 * 60 * 1000,
+}));
+
+
+// var cookieParser = require('cookie-parser');
+// app.use(cookieParser());
 
 const bcrypt = require('bcryptjs');
 
 function generateRandomString(len) {
   var ans = '';
-  const array = 'abcdefghijklmnopqrstuvwxyz123456789123456789123456789';
+  const array = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrtsuvwxyz123456789123456789123456789123456789';
   for (var i = len; i > 0; i--) {
     ans +=
       array[Math.floor(Math.random() * array.length)];
@@ -50,12 +58,18 @@ const urlDatabase =  {};
 const users = {}
 
 app.get('/', (require, response)=>{
-   response.send('Hello!');
+   response.redirect('/login');
 });
 
-app.get('/login', (require, response)=>{
-  const templateVars = {user: users[require.cookies["user_id"]]}
-  response.render('urls_login', templateVars);
+app.get('/login', (require, response) => {
+  if (/*users[require.cookies["user_id"]]*/users[require.session.user_id]) {
+    response.send('Please sign out to log in as a different user ')
+  } else {
+    const templateVars = {
+      user: users[require.session.user_id]/*users[require.cookies["user_id"]]*/
+    }
+    response.render(`urls_login`, templateVars)
+  }
 })
 
 
@@ -66,19 +80,26 @@ app.post('/login', (require, response)=>{
   }else if(bcrypt.compareSync(require.body.password, userInfo.password) === false){
         response.status(403).send("Invalid password")
   }else{
-        response.cookie('user_id', userInfo.id)
+        //response.cookie('user_id', userInfo.id)
+        require.session.user_id = userInfo.id
         response.redirect(`/urls`)
   }
 })
 
 app.post('/logout', (require, response)=>{
-   response.clearCookie('user_id')
+   require.session = null 
    response.redirect(`/login`)
 })
 
-app.get('/register', (require, response)=>{
-   const templateVars = {user: users[require.cookies["user_id"]]}
-   response.render('urls_register', templateVars);
+app.get('/register', (require, response) => {
+  if (/*users[require.cookies["user_id"]]*/users[require.session.user_id]) {
+    response.redirect('/urls')
+  } else {
+    const templateVars = {
+      user: /*users[require.cookies["user_id"]]*/users[require.session.user_id]
+    }
+    response.render('urls_register', templateVars);
+  }
 })
 
 app.post('/register', (require, response) => {
@@ -93,30 +114,30 @@ app.post('/register', (require, response) => {
       email: require.body.email,
       password: bcrypt.hashSync(require.body.password, 10)
     }
-    response.cookie('user_id', userID)
-    console.log(users)
+    // response.cookie('user_id', userID)
+    require.session.user_id = userID
     response.redirect(`/urls`)
   }
 })
 
 app.get('/urls', (require, response) => {
-  if (!users[require.cookies["user_id"]]) {
+  if (/*!users[require.cookies["user_id"]]*/!users[require.session.user_id]) {
     response.send("Please log in or register")
   } else {
     const templateVars = {
-      urls: urlsForUser(require.cookies["user_id"]),
-      user: users[require.cookies["user_id"]]
+      urls: urlsForUser(/*require.cookies["user_id"]*/require.session.user_id),
+      user: users[/*require.cookies["user_id"]*/require.session.user_id]
     }
     response.render('urls_index', templateVars);
   }
 })
 
 app.get('/urls/new', (require, response) => {
-  if (!users[require.cookies["user_id"]]) {
+  if (/*!users[require.cookies["user_id"]]*/!users[require.session.user_id]) {
     response.redirect('/login')
   } else {
     const templateVars = {
-      user: users[require.cookies["user_id"]]
+      user: users[/*require.cookies["user_id"]*/require.session.user_id]
     }
     response.render('urls_new', templateVars)
   }
@@ -124,7 +145,7 @@ app.get('/urls/new', (require, response) => {
 
 app.post('/urls', (require, response)=>{
    const randomString = generateRandomString(6)
-   urlDatabase[randomString] = {longURL: require.body.longURL, userID: users[require.cookies["user_id"]].id}
+   urlDatabase[randomString] = {longURL: require.body.longURL, userID: users[require.session.user_id].id /*users[require.cookies["user_id"]].id*/}
    response.redirect(`/urls/${randomString}`)
 })
 
@@ -135,7 +156,7 @@ app.post('/urls/:id', (require, response)=>{
 })
 
 app.post('/urls/:shortURL/delete', (require, response)=>{
-   if(!users[require.cookies["user_id"]] || urlDatabase[require.params.shortURL].userID !== users[require.cookies["user_id"]].id){
+   if(/*!users[require.cookies["user_id"]]*/!users[require.session.user_id] || urlDatabase[require.params.shortURL].userID !== users[require.session.user_id].id/*users[require.cookies["user_id"]].id*/){
    response.send("Please login as the appropriate user")
  }else{
    const shURL = require.params.shortURL
@@ -145,17 +166,27 @@ app.post('/urls/:shortURL/delete', (require, response)=>{
 })
 
 app.get("/urls/:shortURL", (req, res) => {
-  if(!users[req.cookies["user_id"]] || urlDatabase[req.params.shortURL].userID !== users[req.cookies["user_id"]].id){
-  res.send("Please login as the appropriate user")
- }else{
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies["user_id"]]};
-  res.render("urls_show", templateVars);
- }
+  if (!urlDatabase[req.params.shortURL]) {
+    res.status(404).send("This short URL has not been set")
+  } else if (/*!users[req.cookies["user_id"]]*/!users[req.session.user_id] || urlDatabase[req.params.shortURL].userID !== users[req.session.user_id].id/*users[req.cookies["user_id"]].id*/) {
+    res.status(401).send("Please login as the appropriate user")
+  } else {
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL,
+      user: /*users[req.cookies["user_id"]]*/users[req.session.user_id]
+    };
+    res.render("urls_show", templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL
-  res.redirect(longURL);
+  if (!urlDatabase[req.params.shortURL]) {
+    res.status(302).send("Ooops")
+  } else {
+    const longURL = urlDatabase[req.params.shortURL].longURL
+    res.redirect(longURL);
+  }
 });
 
 app.get('/urls.json', (require, response)=>{
