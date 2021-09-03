bf91a13b59af74c617afcebe430baae6106d054a
siopao2001 <sio_pao2001@yahoo.ca>
2021-09-03 14:15:42 +0000

    Created js file to seperate helper functions to export

---
+++ helpers.js
@@ -0,0 +1,33 @@
+const emailSearch = function(userEmail, database) {
+  for (item in database) {
+    if (database[item].email === userEmail) {
+      return database[item]
+    }
+  }
+  return false
+};
+
+const urlsForUser = function(id, data) {
+  let newObject = {}
+  for (url in data) {
+    if (data[url].userID === id) {
+      newObject[url] = {
+        longURL: data[url].longURL,
+        userID: data[url].userID
+      }
+    }
+  }
+  return newObject
+};
+
+const generateRandomString = function(len) {
+  var ans = '';
+  const array = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrtsuvwxyz123456789123456789123456789123456789';
+  for (var i = len; i > 0; i--) {
+    ans +=
+      array[Math.floor(Math.random() * array.length)];
+  }
+  return ans;
+};
+
+module.exports = {emailSearch, urlsForUser, generateRandomString}
