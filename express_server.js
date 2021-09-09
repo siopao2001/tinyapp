50cdaf9ae46a5a9a8dffbecb7c85b5f01c9a4130
siopao2001 <sio_pao2001@yahoo.ca>
2021-09-09 14:36:20 +0000

    Modify get /u/:id to send proper error message

--- express_server.js
+++ express_server.js
@@ -155,7 +155,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
-    res.status(302).send("Ooops");
+    res.status(404).send("This short URL has not been set");
   } else {
     const longURL = urlDatabase[req.params.shortURL].longURL;
     res.redirect(longURL);
